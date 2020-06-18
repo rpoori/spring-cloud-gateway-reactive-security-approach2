33b70a74022d145f35a6622a2970bf4d3f92cfd7
Poornima Rajendhran <poornimarajendhran@Poornimas-Air.home>
2020-06-18 07:16:29 -0400

    Spring cloud gateway skeleton setup

---
+++ src/main/java/com/my/poc/springcloudgatewayreactivesecurityapproach2/SpringCloudGatewayReactiveSecurityApproach2Application.java
@@ -0,0 +1,13 @@
+package com.my.poc.springcloudgatewayreactivesecurityapproach2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudGatewayReactiveSecurityApproach2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudGatewayReactiveSecurityApproach2Application.class, args);
+	}
+
+}
